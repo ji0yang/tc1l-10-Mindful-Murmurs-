c90ab04a8dbc0fa0c7d82e02b249e669486ed302
jiayang <chiduck2003@gmail.com>
2024-08-29 00:38:20 +0800

    Initialise repository.

---
+++ app.py
@@ -0,0 +1,120 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+from flask_bcrypt import Bcrypt
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'thisisasecretkey'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Register")
+
+    def validate_username(self, username):
+        existing_user = User.query.filter_by(username=username.data).first()
+        if existing_user:
+            raise ValidationError('This username has already been taken. Please choose another one.')
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
+    submit = SubmitField("Login")
+
+class CommentForm(FlaskForm):
+    comment = StringField('Comment', validators=[InputRequired()])
+    submit = SubmitField("Comment")
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+FORBIDDEN_WORDS = ['bullshit', 'shit', 'dumb', 'retard', 'cunt', 'idiot']
+comments = []
+
+def filter_text(text):
+    text_lower = text.lower()
+    for word in FORBIDDEN_WORDS:
+        text_lower = text_lower.replace(word, '*' * len(word))
+    return text_lower
+
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            return redirect(url_for('comment'))
+        flash('Login failed. Check your username and/or password.')
+    return render_template('login.html', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created! You can now log in.')
+        return redirect(url_for('login'))
+    return render_template('register.html', form=form)
+
+@app.route('/comment', methods=['GET', 'POST'])
+@login_required
+def comment():
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = form.comment.data
+        filtered_comment = filter_text(comment)
+        if any(word in comment.lower() for word in FORBIDDEN_WORDS):
+            flash('Your comment contains forbidden words.', 'danger')
+        else:
+            comments.append({'username': current_user.username, 'comment': filtered_comment})
+            flash('Comment added successfully!', 'success')
+        return redirect(url_for('comment'))
+    return render_template('comment.html', form=form, comments=comments)
+
+@app.route('/delete/<int:comment_id>')
+@login_required
+def delete(comment_id):
+    comment = next((comment for comment in comments if comment['username'] == current_user.username and comments.index(comment) == comment_id), None)
+    if comment:
+        comments.remove(comment)
+        flash('Comment deleted successfully!', 'success')
+    else:
+        flash('You do not have permission to delete this comment.', 'danger')
+    return redirect(url_for('comment'))
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
